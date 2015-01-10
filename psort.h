0797fe835ae924771837490b2e38a2370e5625d6
rohitramkumar <rohit.ramkumar@yahoo.com>
2015-01-10 14:34:08 -0800

    Update psort.h

--- psort.h
+++ psort.h
@@ -22,60 +22,60 @@ namespace ParallelSort
     
     /**
      * Function that is called by the concurrent_sort function if the number of elements to sort is less than 50.
-	 * @param array - The structure to sort
-	 * @param lo - represents the low index of a particular section of the array
-	 * @param high - represents the high index of a particular section of the array
+     * @param array - The structure to sort
+     * @param lo - represents the low index of a particular section of the array
+     * @param high - represents the high index of a particular section of the array
      */
     template<class T>
     void sequential_psort(vector<T>* array, int lo, int high);
 
     /**
      * Function that is called by wrapper to execute a parallel quicksort	
-	 * @param array - The structure to sort
-	 * @param lo - represents the low index of a particular section of the array
-	 * @param high - represents the high index of a particular section of the array
+     * @param array - The structure to sort
+     * @param lo - represents the low index of a particular section of the array
+     * @param high - represents the high index of a particular section of the array
      */
     template<class T>
     void concurrent_psort(vector<T>* array, int lo, int high);
 
     /**
      * Helper function that partitions vector based on a pivot value	
-	 * @param array - The structure to partition
-	 * @param lo - represents the low index of a particular section of the array
-	 * @param high - represents the high index of a particular section of the array
-	 * @return int - index of the pivot value
+     * @param array - The structure to partition
+     * @param lo - represents the low index of a particular section of the array
+     * @param high - represents the high index of a particular section of the array
+     * @return int - index of the pivot value
      */
     template <class T>
     int partition(vector<T>* array, int lo, int high);
     
     /**
      * Helper function that produces a random pivot index	
-	 * @param lo - represents the low index of a particular section of the array
-	 * @param high - represents the high index of a particular section of the array
-	 * @return int - the index of the random pivot
+     * @param lo - represents the low index of a particular section of the array
+     * @param high - represents the high index of a particular section of the array
+     * @return int - the index of the random pivot
      */
     int getRandomPivot(int lo, int high);
 
     /**
      * Function that performs an insertion sort
-	 * @param array - the structure to sort
+     * @param array - the structure to sort
      * @param lo - value to keep track of the low bound of structure during sorting
-	 * @param hi - value to keep track of the high bound of structure during sorting
+     * @param hi - value to keep track of the high bound of structure during sorting
      */
     template<class T>
     void insertionSort(vector<T>* array, int lo, int hi);
 
     /**
-     * Function that checks if the array is already sortedi
-	 * @param array - the structure that we want to check 
-	 * @return bool - whether the structure is sorted or not
+     * Function that checks if the array is already sorted
+     * @param array - the structure that we want to check 
+     * @return bool - whether the structure is sorted or not
      */ 
      template <class T>
     bool sorted(vector<T>* array);
 
     /**
      * Helper function that swaps two indices in the given vector.
-	 * @param array - the structure that holds the indices we are swapping
+     * @param array - the structure that holds the indices we are swapping
      * @param index1 - index of the first element we want to swap
      * @param index2 - index of the second element we want to swap
      */
